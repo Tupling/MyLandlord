a1012ccae3c65384100d5fc8cdd11e3cfa47b809
Dale Tupling <tupling@fullsail.edu>
2014-11-17 10:18:45 -0500

    Initial Project Setup

---
+++ MyLandlord/MyLandlord/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  MyLandlord
+//
+//  Created by Dale Tupling on 11/17/14.
+//  Copyright (c) 2014 Dale Tupling. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
