f1a99bb3c3a0a36daffd47546d6684fa90fc1a64
Dale Tupling <tupling@fullsail.edu>
2014-12-18 15:10:06 -0500

    Added dropbox link unlink ability

--- MyLandlord/MyLandlord/MLSettings.h
+++ MyLandlord/MyLandlord/MLSettings.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+#import <DropboxSDK/DropboxSDK.h>
 
 @interface MLSettings : UIViewController
+{
 
+}
+
+@property (nonatomic, strong) IBOutlet UIButton *linkDropBox;
+@property (nonatomic, strong) IBOutlet UILabel *dropBoxStatus;
 @end
