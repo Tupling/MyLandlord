59347f269e4d1b7721e3fb39b54aa826394d0f90
Dale Tupling <tupling@fullsail.edu>
2014-12-18 23:17:22 -0500

    Delete Unit

--- MyLandlord/MyLandlord/MLPropertyUnits.m
+++ MyLandlord/MyLandlord/MLPropertyUnits.m
@@ -15,6 +15,9 @@
     UILabel *unitName;
     UILabel *assignedTenant;
     NSArray *unitArray;
+    
+    UIAlertView *deleteObject;
+    UIAlertView *deleteError;
 }
 
 @end
@@ -87,6 +90,8 @@
 }
 
 
+
+#pragma mark - TableView Delegate Methods
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return [unitArray count];
@@ -159,8 +164,73 @@
     [self performSegueWithIdentifier:@"unitDetails" sender:self];
 
 }
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        self.subUnitDetails = [ApplicationDelegate.subUnitArray objectAtIndex:indexPath.row];
+        
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"subUnitId == %@", self.subUnitDetails.unitObjectId];
+        NSArray *predicateResults = [ApplicationDelegate.tenantsArray filteredArrayUsingPredicate:predicate];
+        
+        NSLog(@"PREDICATE COUNT == %lu", (unsigned long)predicateResults.count);
+        
+        if (predicateResults.count == 0) {
+            deleteObject = [[UIAlertView alloc] initWithTitle:@"Remove Property" message:@"Are you sure you want to delete this property? This operation will not remove the unit folder from your dropbox." delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Yes", nil];
+            
+            //Set alert tag do index path. Allows me to pass the table index of item being deleted.
+            deleteObject.tag = indexPath.row;
+            
+            [deleteObject show];
+        } else {
+            deleteError = [[UIAlertView alloc] initWithTitle:@"Error" message:@"You cannot remove a property that has a current tenant!" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+            
+            [deleteError show];
+        }
+        
+        
+        
+        
+    }
+}
+
+#pragma mark - Alertview Delegate Methods
+//Alert user of actions
+-(void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    if(alertView == deleteObject){
+        
+        //If User selected YES to remove tenant
+        if (buttonIndex == 1) {
+            
+            //Get tenant object from Parse
+            PFObject *subUnit = [PFObject objectWithoutDataWithClassName:@"SubUnits" objectId:self.subUnitDetails.unitObjectId];
+            NSUInteger rowIndex = deleteObject.tag;
+            
+            //Remove this tenant object from tenantsArray
+            [ApplicationDelegate.subUnitArray removeObjectAtIndex:rowIndex];
+            
+            NSArray *vcArray = [self.navigationController viewControllers];
+            
+            [self.navigationController popToViewController:[vcArray objectAtIndex:0] animated:YES];
+
+            
+            [subUnit deleteInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if (succeeded) {
+                    UIAlertView *operationDone = [[UIAlertView alloc] initWithTitle:@"Success" message:@"Property Successfully Removed" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+                    
+                    [operationDone show];
+                }
+            }];
+        }
+    }
+    
+}
 
 
+
+
+
+#pragma mark - Navigation
 //In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     
